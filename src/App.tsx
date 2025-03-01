2ca4452cce18a74802d4708511cdf6ddbeed2d11
hackercry04 <metester560@gmail.com>
2025-03-01 20:13:34 +0530

    Your commit message

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom"; // Change BrowserRouter to HashRouter
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
-import Page from "./app/Page"
+import Page from "./app/Page";
 import AdminLogin from "./Pages/AdminLogin";
 import LoansPage from "./app/adminpage";
 import LoanDetailPage from "./app/admin/loans/details/page";
 import { Toaster } from "react-hot-toast";
 import MessageHandler from "./components/MessageHandler";
+
 function App() {
-  return (<>
-
-    <Router>
-
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Page/>}/>
-          <Route path="/admin/login" element={<AdminLogin/>}/>
-          <Route path="/admin/loans" element={<LoansPage/>}/>
-          <Route path="/admin/loans/detail" element={<LoanDetailPage/>}/>
-        </Routes>
-      </div>
-    </Router>
-  </>
+  return (
+    <>
+      <Router>
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Page />} />
+            <Route path="/admin/login" element={<AdminLogin />} />
+            <Route path="/admin/loans" element={<LoansPage />} />
+            <Route path="/admin/loans/detail" element={<LoanDetailPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
